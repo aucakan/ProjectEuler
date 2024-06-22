6bb0f91b20479a4b32cde72637335994a8f9047b
ASAF UTKAN ÇAKAN <cakanasafutkan@gmail.com>
2024-06-22 13:06:20 +0300

    new sols
    We started our strategy from the bottom row, and at each step, we considered the following: if we are positioned at an element in the second-to-last row, we have two possible numbers to choose to complete our path. Therefore, we will definitely choose the larger one to get a greater sum.

---
+++ Solutions/Problem[0067].py
@@ -0,0 +1,12 @@
+"""
+Project Euler Problem 23
+"""
+
+with open("0067_triangle.txt", "r") as t:
+    tri = t.read().split("\n")[:-1]
+    lst = [[int(j) for j in i.split()] for i in tri]
+    for i in range(1, len(lst.copy())):
+        for j in range(len(lst[-2])):
+            lst[-2][j] += max(lst[-1][j], lst[-1][j + 1])
+        lst.pop(-1)
+    print(lst)
