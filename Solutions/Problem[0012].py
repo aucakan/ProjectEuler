c07b47b591721a40d3c2ca45ddccc1ade6ed5628
ASAF UTKAN ÇAKAN <cakanasafutkan@gmail.com>
2024-06-24 00:24:44 +0300

    Updated Problem[0012]

--- Solutions/Problem[0012].py
+++ Solutions/Problem[0012].py
@@ -1,22 +1,17 @@
 """
 Project Euler Problem 12
 """
-
 def list_factors(x):
-    lst = []
-    xn = round(x**0.5)
-    for i in range(1, xn + 1):
+    factors = set()
+    for i in range(1, round(x**0.5) + 1):
         if x % i == 0:
-            lst.append(i)
-            if x / i not in lst:
-                lst.append(int(x / i))
-    return sorted(lst)
+            factors.add(i)
+            factors.add(int(x / i))
+    return sorted(factors)
 
 tri_nums = [1]
 index = 2
 while len(list_factors(tri_nums[-1])) < 500:
     tri_nums.append(tri_nums[-1] + index)
     index += 1
-print(tri_nums[-1])
-
-
+print(tri_nums[-1]) #76576500
